16b1475b30e151a98c2db2d6ae6bf1dd1e524b61
Natalie-Cardot <nataliecardot@gmail.com>
2020-08-01 17:35:26 -0700

    Add todo post route

--- routes/todos.js
+++ routes/todos.js
@@ -6,4 +6,11 @@ const router = express_1.Router();
 router.get('/', (req, res, next) => {
     res.status(200).json({ todos });
 });
+router.post('/todo', (req, res, next) => {
+    const newTodo = {
+        id: new Date().toISOString(),
+        text: req.body.text,
+    };
+    todos.push(newTodo);
+});
 exports.default = router;
