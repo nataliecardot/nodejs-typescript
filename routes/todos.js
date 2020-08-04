f0218697cd9247dd5cfd9c29445d1ffa89b00626
Natalie-Cardot <nataliecardot@gmail.com>
2020-08-03 18:02:48 -0700

    Compile code

--- routes/todos.js
+++ routes/todos.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
-const todos = [];
+let todos = [];
 const router = express_1.Router();
 router.get('/', (req, res, next) => {
     res.status(200).json({ todos });
@@ -12,5 +12,21 @@ router.post('/todo', (req, res, next) => {
         text: req.body.text,
     };
     todos.push(newTodo);
+    // 201: created success
+    return res.status(201).json({ message: 'Todo added.', todo: newTodo, todos });
+});
+// Replace todo
+router.put('/todo/:todoId', (req, res, next) => {
+    const todoId = req.params.todoId;
+    const todoIndex = todos.findIndex((todoItem) => todoItem.id === todoId);
+    if (todoIndex >= 0) {
+        todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+        return res.status(200).json({ message: 'Todo updated.', todos });
+    }
+    res.status(404).json({ message: 'Could not find todo with this ID.' });
+});
+router.delete('/todo/:todoId', (req, res, next) => {
+    todos = todos.filter((todoItem) => todoItem.id !== req.params.todoId);
+    res.status(200).json({ message: 'Todo deleted.', todos });
 });
 exports.default = router;
