ab16b2a8b9950d0fcec898844da7263ca8ca2d11
Natalie-Cardot <nataliecardot@gmail.com>
2020-08-06 19:52:17 -0700

    Use type casting

--- routes/todos.ts
+++ routes/todos.ts
@@ -2,6 +2,9 @@ import { Router } from 'express';
 
 import { Todo } from '../models/todo';
 
+type RequestBody = { text: string };
+type RequestParams = { todoId: string };
+
 let todos: Todo[] = [];
 
 const router = Router();
@@ -11,9 +14,10 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/todo', (req, res, next) => {
+  const body = req.body as RequestBody;
   const newTodo: Todo = {
     id: new Date().toISOString(),
-    text: req.body.text,
+    text: body.text,
   };
 
   todos.push(newTodo);
@@ -24,17 +28,20 @@ router.post('/todo', (req, res, next) => {
 
 // Replace todo
 router.put('/todo/:todoId', (req, res, next) => {
-  const todoId = req.params.todoId;
+  const params = req.params as RequestParams;
+  const todoId = params.todoId;
+  const body = req.body as RequestBody;
   const todoIndex = todos.findIndex((todoItem) => todoItem.id === todoId);
   if (todoIndex >= 0) {
-    todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+    todos[todoIndex] = { id: todos[todoIndex].id, text: body.text };
     return res.status(200).json({ message: 'Todo updated.', todos });
   }
   res.status(404).json({ message: 'Could not find todo with this ID.' });
 });
 
 router.delete('/todo/:todoId', (req, res, next) => {
-  todos = todos.filter((todoItem) => todoItem.id !== req.params.todoId);
+  const params = req.params as RequestParams;
+  todos = todos.filter((todoItem) => todoItem.id !== params.todoId);
   res.status(200).json({ message: 'Todo deleted.', todos });
 });
 
